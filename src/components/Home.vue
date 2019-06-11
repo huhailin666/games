f59a8cf958e2b63638a8dbdd52a6301820e6a4f7
xiaohai <857919293@qq.com>
2019-06-12 00:36:40 +0800

    完成首页美化

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,39 +2,79 @@
   <div>
     <div class="container">
       <h1>请选择你想玩的游戏</h1>
-        <a href="#/card">蜘蛛纸牌</a>
-        <a href="#/mine">扫雷</a>
-      <div class="img">
-        <a href="#/card">
-          <img src="../../static/images/card.jpg" alt>
-        </a>
-
-        <a href="#/mine">
-          <img src="../../static/images/mine.jpg" alt>
-        </a>
+      <a class="button" href="#/card" @mouseenter="xxx=false">蜘蛛纸牌</a>
+      <a class="button" href="#/mine" @mouseenter="xxx=true">扫雷</a>
+      <div class="img" :class="{xxx:xxx}">
+        <div class="img-cotainer">
+          <a href="#/card">
+            <img src="../../static/images/card.jpg" alt>
+          </a>
+          <a href="#/mine">
+            <img src="../../static/images/mine.jpg" alt>
+          </a>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      xxx: true
+    };
+  }
+};
 </script>
 <style lang="less" scoped>
 .container {
+  background: #eee;
   text-align: center;
   vertical-align: center;
-  a {
+  min-height: 100vh;
+  h1 {
+    border-bottom: 1px solid red;
+    padding: 20px 0;
+    box-shadow: -3px 9px 3px -3px rgba(210, 91, 91, 0.75);
+    margin-bottom: 20px;
+  }
+  .button {
+    display: inline-block;
+    font-size: 20px;
     margin: 10px;
+    background: #69cc69;
+    color: #fff;
+    // border: 1px solid #ccc;
+    padding: 10px 20px;
+    margin: 10px 60px;
+    border-radius: 30px;
+    width: 200px;
+    box-shadow: 0px 0px 3px 5px rgba(222, 204, 204, 0.75);
   }
-  img {
-    margin: 20px;
-  }
-  img:first-child {
-    width: 300px;
-    height: 300px;
-  }
-  img:last-child {
-    width: 300px;
+  .img {
+    width: 500px;
+    height: 500px;
+    margin: 0 auto;
+    border-radius: 20px;
+    box-shadow: 3px 3px 11px 1px rgba(155, 215, 150, 0.75);
+    overflow: hidden;
+    .img-cotainer {
+      width: 200%;
+      height: 100%;
+      img {
+        width: 497px;
+        height: 100%;
+      }
+              transition: all 0.5s;
+        transform: translateX(0);
+
+    }
+    &.xxx {
+      .img-cotainer {
+
+        transform: translateX(-50%);
+      }
+    }
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ Vue.config.productionTip = false
 // import {Button,Toast} from 'mint-ui'
 // Vue.component(Toast.name,Toast);
 // Vue.component(Button.name, Button);
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 import './lib/animate/animate.css'
 import './lib/common/common.css'
 
